b795702a5acc62bd817bde4ae2dd56574f4c04fe
hasen <hasen@LAPTOP-DVTUEKF2>
2022-09-01 17:39:43 +0300

    Добавьте файлы проекта.

---
+++ GrpcBillingService/Program.cs
@@ -0,0 +1,21 @@
+using GrpcBillingService;
+using GrpcBillingService.Interfaces;
+using GrpcBillingService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddSingleton<IBillingDataService, DefaultData>();
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<BillingService>();
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
